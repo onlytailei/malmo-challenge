95ad363bc9b9ec884a55f7677bc118f63bba094d
Lei Tai <onlytailei@hotmail.com>
2017-04-04 18:48:22 +0200

    train with A3C

--- ai_challenge/pig_chase/start.sh
+++ ai_challenge/pig_chase/start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-cd ~/Freiburg/Malmo/Minecraft/
-./launchClient.sh -port 10000 &
-./launchClient.sh -port 10001 &
+#cd ~/Freiburg/Malmo/Minecraft/
+#./launchClient.sh -port 10000 &
+#./launchClient.sh -port 10001 &
+ps -ef | grep 'python -m visdom.server' | grep -v grep | awk '{print $2}' | xargs kill
+python -m visdom.server &
+OMP_NUM_THREADS=1 python pig_chase_a3c.py 
