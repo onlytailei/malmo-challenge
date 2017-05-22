110301163232c4aa15c7d6fa71f512b141fe07fe
Lei Tai <onlytailei@hotmail.com>
2017-05-22 17:25:54 +0200

    * delete useless file

--- ai_challenge/pig_chase/start.sh
+++ ai_challenge/pig_chase/start.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-#cd ~/Freiburg/Malmo/Minecraft/
-#./launchClient.sh -port 10000 &
-#./launchClient.sh -port 10001 &
 ps -ef | grep 'python -m visdom.server' | grep -v grep | awk '{print $2}' | xargs kill
 python -m visdom.server &  
 OMP_NUM_THREADS=1 python pig_chase_a3c.py \
@@ -38,6 +35,3 @@ OMP_NUM_THREADS=1 python pig_chase_a3c.py \
     malmo31:10000 \
     malmo32:10000 \
     --num-processes 16
-   # > ./stderr/stderr_swarm_out 2>&1
-#OMP_NUM_THREADS=1 python pig_chase_a3c.py malmo1:10000 malmo2:10000
-#OMP_NUM_THREADS=1 python pig_chase_a3c.py localhost:10000 localhost:10001
